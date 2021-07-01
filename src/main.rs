93da05a5f510a2debcb4bd828e39271c7655c807
Orefa <Orefa@foxmail.com>
2021-07-01 22:04:32 +0800

    get first Macaddress and list MacAddress

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use mac_address::{self, get_mac_address};
+fn main() {
+	match get_mac_address() {
+        Ok(Some(ma)) => {
+            println!("MAC addr = {}", ma);
+            println!("bytes = {:?}", ma.bytes());
+        }
+        Ok(None) => println!("No MAC address found."),
+        Err(e) => println!("{:?}", e),
+    }
+	
+	match mac_address::MacAddressIterator::new() {
+		Ok(iter) => {
+			for ele in iter {
+				println!("{}", ele);
+			}
+			
+		}
+		Err(e) => println!("{:?}", e),
+	}
+}
